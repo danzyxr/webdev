b9f0e69ec7479533bc8405d6c5bc5deb332b4da1
danzyxr <49037253+danzyxr@users.noreply.github.com>
2019-10-18 03:26:29 -0500

    Just playing

--- myScript.js
+++ myScript.js
@@ -1,2 +1,27 @@
-console.log("hello, world");
-console.log("Testing git stuff!");
+// console.log("hello, world");
+// console.log("Testing git stuff!");
+
+// let result = 1;
+// let counter = 0;
+// while (counter < 10) {
+//     result = result * 2;
+//     counter = counter + 1;
+// }
+// console.log(result);
+
+// let yourName;
+// do {
+//     yourName = prompt("Who are you?");
+// } while (!yourName);
+// console.log(yourName);
+
+// if (false != true) {
+//     console.log("That makes sense");
+//     if (1 < 2) {
+//         console.log("No surprise there");
+//     }
+// }
+
+for (let i = 0; i < 13; i++) {
+    console.log(i)
+}
